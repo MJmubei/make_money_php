9c2e472a187e1a3d3c347dae8cfc4b58303b2a2b
revert-yang <kan.yang@starcor.com>
2018-12-03 21:11:04 +0800

    wechat notify scan

--- api/notify_pay/we_chat/notify_scan.php
+++ api/notify_pay/we_chat/notify_scan.php
@@ -7,20 +7,24 @@
 */
 header("content-type:text/html;charset=utf-8");
 
-//引入文件
-
-include_once dirname(dirname(dirname(__DIR__))) . '/index.php';
-include_once dirname(dirname(dirname(__DIR__))) . '/app_model/libraries/em_return.class.php';
-
-
+//日志路径
+$str_olg_file_path = 'api/notify_pay/we_chat';
+em_return::set_ci_flow_desc($str_olg_file_path,'微信扫码/JSAPI支付回调:异步通知开始','message','info');
 //提取回调数据
 $obj_notify_data = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : file_get_contents('php://input');
-
+em_return::set_ci_flow_desc($str_olg_file_path,'微信扫码/JSAPI支付回调:接收微信回调原始数据:' . $obj_notify_data,'message','info');
 //解析回调数据
 libxml_disable_entity_loader(true);
 $arr_notify_data = json_decode(json_encode(simplexml_load_string($obj_notify_data, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
 if(!isset($arr_notify_data) || empty($arr_notify_data))
 {
-    em_return::set_ci_flow_desc();
+    em_return::set_ci_flow_desc($str_olg_file_path,'微信扫码/JSAPI支付回调:解析微信回调原始数据失败','message','error');
+    return false;
 }
+unset($obj_notify_data);
+
+
+
+
+
 
