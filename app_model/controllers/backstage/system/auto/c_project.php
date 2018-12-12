a38af838fe9161a3397275229c9f71c62fdfcf92
MJmubei <www.lp285474183.co@qq.com>
2018-12-12 11:04:59 +0800

    SQL

--- app_model/controllers/backstage/system/auto/c_project.php
+++ app_model/controllers/backstage/system/auto/c_project.php
@@ -17,56 +17,26 @@ class c_project extends CI_Controller
     }
     
     /**
-     * 列表页
+     * 树形图
      */
     public function tree()
     {
         $this->load_view_file(array('menu_fenlei'=>$this->CI_make_child()),__LINE__);
     }
     
-    
+    /**
+     * 树形操作图
+     */
     public function classify()
     {
         $this->load_view_file(array('menu_fenlei'=>$this->CI_make_child()),__LINE__);
     }
     
-    public function CI_make_child($level = 1,$parent=0)
-    {
-        $last_data=$temp_data=null;
-        $num = rand(1, 6);
-        if($level >3)
-        {
-            return null;
-        }
-        for ($i=0;$i<$num;$i++)
-        {
-            $id = em_guid::em_guid_rand();
-            $last_data[] = array(
-                'text'=>$level."级分类[{$i}]",
-                'text1111'=>$level."级分类[{$i}]",
-                'levels'=>1,
-                'tags'=>array(
-                    '<div onclick="my_alert(\'添加子分类\');">添加子分类</div>',
-                    '<div onclick="my_alert(\'修改\');">修改</div>',
-                    '<div onclick="my_alert(\'删除\');">删除</div>',
-                    '<div onclick="my_alert(\'置顶\');">置顶</div>',
-                    '<div onclick="my_alert(\'上移\');">上移</div>',
-                    '<div onclick="my_alert(\'下移\');">下移</div>',
-                    '<div onclick="my_alert(\'置底\');">置底</div>',
-                ),
-                'cms_name'=>$level."级分类[{$i}]",
-                'cms_parent_id'=>$parent,
-                'cms_id'=>$id,
-                'cms_order'=>0.00,
-                'nodes'=>$this->CI_make_child($level+1,$id),
-            );
-        }
-        return $last_data;
-    }
+    
 
     
     /**
-     * 修改页面
+     * 添加
      */
     public function add()
     {
@@ -76,7 +46,7 @@ class c_project extends CI_Controller
     
     
     /**
-     * 修改页面
+     * 修改
      */
     public function edit()
     {
@@ -98,7 +68,7 @@ class c_project extends CI_Controller
     
     
     /**
-     * delete页面
+     * 删除
      */
     public function delete()
     {
@@ -114,6 +84,26 @@ class c_project extends CI_Controller
         );
         $this->load_view_file($this->auto_load_table('system','auto','c_project','system_project', 'delete',$delete_params),__LINE__);
     }
+    
+    
+    
+    public function state()
+    {
+        $cms_id = isset($this->arr_params['cms_id']) ? $this->arr_params['cms_id'] : null;
+        if(empty($cms_id) && !is_array($cms_id))
+        {
+            $this->load_view_file(em_return::return_data(1,'删除参数条件为空'),__LINE__);
+        }
+        $delete_params = array(
+            'where'=>array(
+                'cms_id'=>$cms_id,
+            ),
+        );
+        $this->load_view_file($this->auto_load_table('system','auto','c_project','system_project', 'delete',$delete_params),__LINE__);
+    }
+    
+    
+    
     
     /**
      * 自动生成项目
@@ -162,5 +152,40 @@ class c_project extends CI_Controller
         }
         $result = $this->auto_load_table('system', 'auto', 'c_project', 'system_project', 'delete', array('where' => array('cms_state' => 1)));
         $this->load_view_file($result,__LINE__);
+    }
+    
+    
+    public function CI_make_child($level = 1,$parent=0)
+    {
+        $last_data=$temp_data=null;
+        $num = rand(1, 6);
+        if($level >3)
+        {
+            return null;
+        }
+        for ($i=0;$i<$num;$i++)
+        {
+            $id = em_guid::em_guid_rand();
+            $last_data[] = array(
+                'text'=>$level."级分类[{$i}]",
+                'text1111'=>$level."级分类[{$i}]",
+                'levels'=>1,
+                'tags'=>array(
+                    '<div onclick="my_alert(\'添加子分类\');">添加子分类</div>',
+                    '<div onclick="my_alert(\'修改\');">修改</div>',
+                    '<div onclick="my_alert(\'删除\');">删除</div>',
+                    '<div onclick="my_alert(\'置顶\');">置顶</div>',
+                    '<div onclick="my_alert(\'上移\');">上移</div>',
+                    '<div onclick="my_alert(\'下移\');">下移</div>',
+                    '<div onclick="my_alert(\'置底\');">置底</div>',
+                ),
+                'cms_name'=>$level."级分类[{$i}]",
+                'cms_parent_id'=>$parent,
+                'cms_id'=>$id,
+                'cms_order'=>0.00,
+                'nodes'=>$this->CI_make_child($level+1,$id),
+            );
+        }
+        return $last_data;
     }
 }
